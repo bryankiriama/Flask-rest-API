5a21b429f29596226aef9905d1b2643c4b9026fe
bryankiriama <bryankiriama@gmail.com>
2026-01-05 10:52:38 +0300

    intialized database

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
-from models import db, User, Post
+from models import *
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rest-api.db'
+
+Migrate(app, db)
+db.init_app(app)
 
 @app.route('/')
 def index():
