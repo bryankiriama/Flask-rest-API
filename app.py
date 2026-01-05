aee8046d95c5ca6242806f74237021c6b03101bd
bryankiriama <bryankiriama@gmail.com>
2026-01-05 10:33:42 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"<h1>Welcome to the Flask App!</h1>"
+
+if __name__ == '__main__':
+    app.run(port=5500, debug=True)
---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+#models
+class User(db.Model):
+    __tablwename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+    class Post(db.Model):
+        __tablename__ = 'posts'
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(200), nullable=False)
+        Title = db.Column(db.Text, nullable=False)
+        post_image = db.Column(db.String(200), nullable=True)
